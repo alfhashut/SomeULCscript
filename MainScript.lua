36c7bfc986923721788e8ba6923234101f3b8df0
alfhashut <82755821+alfhashut@users.noreply.github.com>
2023-10-08 21:32:19 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -7,11 +7,14 @@ local ItemsToDelete = {
 
 local attacksFolder = game.Workspace:FindFirstChild("attacks")
 
-for _, item in pairs(attacksFolder:GetChildren()) do
-    for _, name in pairs(ItemsToDelete) do
-        if item.Name == name then
-            item:Destroy()
-            break
+while true do -- Bu döngü sürekli çalışır
+    for _, item in pairs(attacksFolder:GetChildren()) do
+        for _, name in pairs(ItemsToDelete) do
+            if item.Name == name then
+                item:Destroy()
+                break
+            end
         end
     end
+    wait(1) -- Bu komut scripti 1 saniye bekletir
 end
