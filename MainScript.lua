5bfa61a548fe220c785b299efe689a02513316d7
alfhashut <82755821+alfhashut@users.noreply.github.com>
2023-10-09 15:38:47 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -11,14 +11,21 @@ local ItemsToDelete = {
 
 local AttacksFolder = game.Workspace:FindFirstChild("Attacks")
 
-delay(0, function()
-    while true do
-        for _, item in pairs(AttacksFolder:GetChildren()) do
-            for _, name in pairs(ItemsToDelete) do
-                if item.Name == name then
-                    item:Destroy()
-                    break
-                end
+-- RunService modülünü al
+local RunService = game:GetService("RunService")
+
+-- RenderStepped olayına bir işlev bağla
+RunService.RenderStepped:Connect(function()
+    -- AttacksFolder'ın altındaki tüm nesneleri döngüye al
+    for _, item in pairs(AttacksFolder:GetChildren()) do
+        -- ItemsToDelete listesindeki tüm isimleri döngüye al
+        for _, name in pairs(ItemsToDelete) do
+            -- Eğer nesnenin adı listedeki bir isimle eşleşiyorsa
+            if item.Name == name then
+                -- Nesneyi yok et
+                item:Destroy()
+                -- Döngüden çık
+                break
             end
         end
     end
