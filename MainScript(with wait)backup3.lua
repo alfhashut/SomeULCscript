6ee9d43697f2061ee5d709b42d4431d2b9eadff3
alfhashut <82755821+alfhashut@users.noreply.github.com>
2023-10-10 14:49:03 +0300

    Add files via upload

---
+++ MainScript(with wait)backup3.lua
@@ -0,0 +1,24 @@
+local ItemsToDelete = {
+    "CharaSlashModel",
+    "KnifeWall",
+    "KnifeRingModel",
+    "StorySlashModel",
+    "TouchIt",
+    "Warn",
+    "FakeSoul",
+    "SoulRingModel"
+}
+
+local AttacksFolder = game.Workspace:FindFirstChild("Attacks")
+
+while true do
+    for _, item in pairs(AttacksFolder:GetChildren()) do
+        for _, name in pairs(ItemsToDelete) do
+            if item.Name == name then
+                item:Destroy()
+                break
+            end
+        end
+    end
+    wait(0.03)
+end
