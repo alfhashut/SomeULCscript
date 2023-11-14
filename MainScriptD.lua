cefd4532cb66b1c4b3d3865469e2efdbb2fb9e28
alfhashut <82755821+alfhashut@users.noreply.github.com>
2023-11-14 14:43:58 +0300

    Add files via upload

---
+++ MainScriptD.lua
@@ -0,0 +1,27 @@
+local ItemsToDelete = {
+    "DustBoneModel",
+    "DustBlasterModel",
+    "DustZoneModel"
+}
+
+local AttacksFolder = game.Workspace:FindFirstChild("Attacks")
+
+-- RunService modülünü al
+local RunService = game:GetService("RunService")
+
+-- RenderStepped olayına bir işlev bağla
+RunService.RenderStepped:Connect(function()
+    -- AttacksFolder'ın altındaki tüm nesneleri döngüye al
+    for _, item in pairs(AttacksFolder:GetChildren()) do
+        -- ItemsToDelete listesindeki tüm isimleri döngüye al
+        for _, name in pairs(ItemsToDelete) do
+            -- Eğer nesnenin adı listedeki bir isimle eşleşiyorsa
+            if item.Name == name then
+                -- Nesneyi yok et
+                item:Destroy()
+                -- Döngüden çık
+                break
+            end
+        end
+    end
+end)
